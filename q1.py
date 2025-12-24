91e7e5496ec1482fe89ce4d58e69f58196d0649a
mohamed amr <mohamed.amr9327@gmail.com>
2025-12-24 20:08:13 +0200

    AFA mohamed amr

--- q1.py
+++ q1.py
@@ -2,24 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-
-
+# ------------Transformation parameters---------------------
 original = [[10,9], [16,9], [6,17]]
 shift1 = [[-5,4.5], [-8, 4.5],  [-3,8.5]]
 shift2 = [ [-4.5,-5], [-4.5,-8],  [-8.5,-3]]
 
+# ----------------points-------------------------
 outer_dofda3 =[[2,2],[2,3], [4,3], [2,4], [2,5], [3,6], [4,6], [3,8], [3,11],
     [6,15], [6,17], [8,19], [11,19], [12,17], [12,16], [14,16],
     [14,17], [15,19], [18,19], [20,17],[20,15], [23,11], [23,8], 
     [22,6], [23,6], [24,5], [24,4], [22,3], [24,3], [24,2], [2,2]]
 
-batn_dofda3 = [
-    [4,6], [9,2], [8,5], [8,8], [10,9],[16,9], [18,8],[18,5], [17,2], [22,6]
-]
+batn_dofda3 = [[4,6], [9,2], [8,5], [8,8], [10,9],[16,9], [18,8],[18,5], [17,2], [22,6]]
 
-bo2_dofda3 = [
-    [6,13], [7,12], [19,12], [20,13]
-]
+bo2_dofda3 = [[6,13], [7,12], [19,12], [20,13]]
 
 
 dofda3_eye_left = [[8,14], [7,15], [7,17], [8,18], [10,18], [11,17], [11,15] , [10,14], [8,14]  ]
@@ -31,7 +27,7 @@ dofda3_eye_pupil_right = [[16,16], [16,17], [17,17], [17,16], [16,16] ]
 
 dofda3 = [outer_dofda3, bo2_dofda3, batn_dofda3, dofda3_eye_left, dofda3_eye_right, dofda3_eye_pupil_left, dofda3_eye_pupil_right]
 
-
+# =========================transformation==========================
 src_points =np.float32(original)
 dest_points1 = np.float32(shift1)
 dest_points2 = np.float32(shift2)
@@ -46,11 +42,8 @@ transformation_matrix2 = np.delete(transformation_matrix2, 2, axis=1)
 reflected_dofda3 = [[] for i in range(len(dofda3))]
 rotated_dofda3 =[[] for i in range(len(dofda3))]
 
-print(transformation_matrix1)
-print(transformation_matrix2)
 
-
-
+# -------------------------show outline------------------------------------
 def show_outline(outline):
 
     x_cooordinates = [p[0] for p in outline]
@@ -60,6 +53,7 @@ def show_outline(outline):
     plt.plot(x_cooordinates, y_coordinates , 'black', linewidth=2)
 
 
+# -----------------------plotting-----------------------------------------
 def graph():
     plt.title('Plotting from a 2D List')
     plt.axvline(0,color="black")
@@ -80,8 +74,6 @@ def graph():
     plt.legend()
     plt.grid(True)
 
-
-
 for i in range(len(dofda3)):
     shape = np.array(dofda3[i])
     for point in shape :
@@ -96,13 +88,7 @@ for i in range(len(dofda3)):
     show_outline(rotated_dofda3[i])
 
 
-
-
-
 graph()
-
-
-
 plt.show()
 
 
